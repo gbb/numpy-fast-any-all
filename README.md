4e82ccebcead121da1613c4c0f62de0f80c38c6e
Graeme B Bell <grb@skogoglandskap.no>
2013-09-04 10:50:06 +0200

    comments & todo fixed

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 fast_any_all
 ----
 
-Basically a faster version of numpy.any() using numpy.logical_or().
+Basically a 15x faster implementation of a common use case for numpy.any() using numpy.logical_or().
+Implementation is trivial. 
 
-Implementation is trivial.
+
+Usage
+---
+
+import fast_any_all as faa
+faa.any([2d boolean array]), returns true where at least one element is true in axis 0
+faa.all([2d boolean array]), return true where all elements are true in axis 0
+
+
+Examples
+---
+
+See BENCHMARK.md
+
+
+Todo
+---
+
+I think it's possible to make this implement more of the functionality of np.any by using reshape to alter which axis is used.
+Not sure. It's super fast and syntactically nice for my own use case, which is what I care about.
--- fast_any_all.py
+++ fast_any_all.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 # Purpose: 
-# np.any and np.all are quite slow - about 10-20x slower than using logical_or and logical_and calls
+# np.any and np.all are quite slow - about 15x slower than using logical_or and logical_and calls
 # This code tries to combine the best features of np.logical_and/or and np.any/all. 
 # It takes lists of arrays (like np.any/all), but is fast like np.logical_or/and
 # It doesn't have the flexibility (in terms of axis choice) that is offered by np.any/all. 
-# Benchmarking suggested there would be no performance advantage to using inplace np.logical_or/and.
 
 def all(inputs):
   return fast_logic(inputs,np.logical_and)
@@ -14,7 +13,7 @@ def any(inputs):
   return fast_logic(inputs,np.logical_or)
 
 def fast_logic(inputs, logic_func): 
-  # No type-checking; use logical_or/and to check if the arrays are of the correct type (boolean or subtype)
+  # No type-checking; depend on logical_or/and to check if the arrays are boolean or acceptable subtype
 
   # Catch empty input, retain compatibility with np.any return value. 
   if len(inputs)==0: 
@@ -23,6 +22,6 @@ def fast_logic(inputs, logic_func):
   output = inputs[0] # separate out first element
 
   for w in range(1,len(inputs)):   # logical combination with other elements
-    output=logic_func(output,inputs[w])
+    output=logic_func(output,inputs[w])   # note that this loop doesn't run when len(inputs)=1.
   
   return output
